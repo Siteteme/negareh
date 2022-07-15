c499140ce8db254d5bef7bb30d2020b4d9a50cc7
farid <siteteme@gmail.com>
2022-07-15 16:29:39 +0430

    carusel article is added

---
+++ assets/js/main.js
@@ -0,0 +1,16 @@
+// slider
+
+
+let swiper = new Swiper(".mySwiper", {
+    slidesPerView: 3,
+    spaceBetween: 30,
+    loop: true,
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+    },
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+    },
+});
