b0cf6d6e4684aa1d7134d03485017ba19eea4a4a
farid <siteteme@gmail.com>
2022-07-20 04:04:38 +0430

    logo carusel is adedd

--- assets/js/main.js
+++ assets/js/main.js
@@ -14,3 +14,23 @@ let swiper = new Swiper(".mySwiper", {
         prevEl: ".swiper-button-prev",
     },
 });
+
+
+// slider
+
+
+let brand = new Swiper(".myBrands", {
+    slidesPerView: 6,
+    spaceBetween: 0,
+    loop: true,
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+    },
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+    },
+});
+
+
